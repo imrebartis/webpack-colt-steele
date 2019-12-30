c8bae2506e8f9f89b032704ce3971ec12194e2b1
Imre Bartis <imre.bartis@siili.com>
2019-12-30 19:26:42 +0200

    split dev & production

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,8 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  output: {
-    filename: "main.[contentHash].js",
-    path: path.resolve(__dirname, "dist")
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html"
@@ -18,7 +13,7 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          "style-loader", //3. Injects styles into DOM
+          "style-loader", //3. Inject styles into DOM
           "css-loader", //2. Turns css into commonjs
           "sass-loader" //1. Turns sass into css
         ]
