0e4e24cfcaf2be4dbf9b5bb4c47f3cb79d045f36
Imre Bartis <imre.bartis@siili.com>
2019-12-31 14:40:21 +0200

    html-loader, file-loader & clean webpack plugin

--- webpack.common.js
+++ webpack.common.js
@@ -13,10 +13,25 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          "style-loader", //3. Inject styles into DOM
+          "style-loader", //3. Injects styles into DOM
           "css-loader", //2. Turns css into commonjs
           "sass-loader" //1. Turns sass into css
         ]
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "imgs",
+            esModule: false
+          }
+        }
       }
     ]
   }
