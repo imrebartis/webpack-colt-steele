5455796e257d32502c46fbbf6348e7bb0cf283d1
Imre Bartis <imre.bartis@siili.com>
2020-01-03 07:06:53 +0200

    extract & minify CSS

--- webpack.common.js
+++ webpack.common.js
@@ -1,3 +1,4 @@
+
 const path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
@@ -6,21 +7,8 @@ module.exports = {
     main: "./src/index.js",
     vendor: "./src/vendor.js"
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html"
-    })
-  ],
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          "style-loader", //3. Injects styles into DOM
-          "css-loader", //2. Turns css into commonjs
-          "sass-loader" //1. Turns sass into css
-        ]
-      },
       {
         test: /\.html$/,
         use: ["html-loader"]
